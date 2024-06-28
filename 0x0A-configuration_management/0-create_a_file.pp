b04f0437b4f99428cc9862e63dcacf8092d4dc6b
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-06-28 01:12:01 -0700

    update

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# making a file in/tmp using puppet
+
+file { 'kevin_mogakwe':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
