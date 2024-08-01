1e6b2270865daf7c5e4cfeb8dd80161bdf03a7ad
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-08-01 00:25:18 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""this will Export API data to CSV format"""
+"""Export API data to CSV format"""
 import csv
 import requests
 import sys
 
+
 def export_to_csv(user_id):
-    
     user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     user_response = requests.get(user_url)
     
@@ -16,15 +16,12 @@ def export_to_csv(user_id):
     user_data = user_response.json()
     username = user_data.get('username')
 
-    
     todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
 
-
     csv_filename = f"{user_id}.csv"
 
-    
     with open(csv_filename, mode='w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos_data:
@@ -36,6 +33,7 @@ def export_to_csv(user_id):
             ])
 
     print(f"Data for user {user_id} has been written to {csv_filename}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
