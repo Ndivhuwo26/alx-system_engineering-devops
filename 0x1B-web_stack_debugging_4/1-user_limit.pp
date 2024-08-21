38af03baef0e9207e9baa85fb273afd99dfc311d
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-08-21 08:09:25 +0000

    update

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+#this will Change the configuration of OS so that it can possible to login with the
+#holberton user and able to open mutiple files without any errors.
+
+exec { 'increase-hard-file-limit-holberton-user':
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
